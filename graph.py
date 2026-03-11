6d27ef788bdf4c9854803b7aef3ced8a6fee20a3
Naod Abebe <naodabeben@gmail.com>
2026-03-11 07:54:46 +0300

    exp: n-trials 50

--- graph.py
+++ graph.py
@@ -3,3 +3,10 @@
 neighborhood_range = ((2, 3), (2, 5), (3, 5), (5, 8))
 neighborhood_range_patterns = ((82, 85), (77, 80), (81, 92), (78, 84))
 neighborhood_range_runtimes = ((32, 36), (40, 31), (39, 31), (35, 30))
+
+
+#one duplicate found on 300, 
+n_neighborhood = (50, 100, 200, 300, 500, 1000)
+n_neighborhood_patterns = (90, 82, 77, 90, 83, 116)
+n_neighborhood_runtimes = (36, 28, 31, 31, 32, 63)
+n_neighborhood_patternspersec = (2.5, 2.93, 2.48, 2.90, 1.84)
